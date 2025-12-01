5a2297e881466b69526ca4caf06fcaa288bd28f4
safwanhashmi <hashmisaf@outlook.com>
2025-12-01 10:24:33 +0500

    Update default port to 80

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const bcrypt = require('bcryptjs');
 const { db, initDb } = require('./src/db');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 80;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
